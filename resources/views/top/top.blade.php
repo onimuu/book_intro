96563742dcef5d95750ce19a34909d063852d5bd
onimu <onimu16@gmail.com>
2019-05-19 17:47:31 +0900

    toppage create

---
+++ resources/views/top/top.blade.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="utf-8">
+<title>かたるば</title>
+<link rel="stylesheet" href="/css/reset.css">
+<link rel="stylesheet" href="/css/styles.css">
+</head>
+<body>
+  <!-- <header class="header">
+    <div class="flex">
+      <div class="left">
+        <h1><a class="title" href="#">かたるば</a></h1>
+      </div>
+      <div class="right flex">
+        <a class="login" href="#">ログイン</a>
+        <a class="signup" href="#">新規登録</a>
+      </div>
+    </div>
+  </header> -->
+  <section class="mv">
+    <div class="wrapper">
+      <h1 class="title">かたるば</h1>
+      <p class="message">好きな本を語ろう。</p>
+      <div class="buttons">
+        <a class="btn login" href="#">ログイン</a>
+        <a class="btn register" href="#">新規登録</a>
+      </div>
+    </div>
+
+  </section>
+  <section class="explain">
+    <h2 class="question">かたるばとは？</h2>
+    <div class="answer">
+      <p>好きな本を紹介できる投稿サイト。</p>
+      <p>お気に入りの本をシェアしたり、</p>
+      <p>タイムラインで新しい良本を見つけよう。</p>
+    </div>
+  </section>
+  <footer class="footer">
+    <p>© かたるば</p>
+  </footer>
+</body>
+</html>
