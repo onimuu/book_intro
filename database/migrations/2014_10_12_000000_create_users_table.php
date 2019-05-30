d1d51db9b37d754bc4254476d42b2746e826f10a
onimu <onimu16@gmail.com>
2019-05-30 14:50:41 +0900

    file upload smartphone change

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,7 +19,7 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->text('image')->nullable();
+            $table->longText('image')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
